42ae9d1df422a52af6319cddef17db3881b6fbe0
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 09:27:13 -0300

    feat: "criacao da classe Categoria"

--- src/main/java/org/example/Categoria.java
+++ src/main/java/org/example/Categoria.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Categoria {
+    private String nome;
+
+    public Categoria(String nome) {
+        this.setNome(nome);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
