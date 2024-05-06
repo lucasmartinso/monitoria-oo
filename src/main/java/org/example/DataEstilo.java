d7aa3dc94f3b9dfd133623ab65cf5c0f76696ab4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 12:35:08 -0300

    feat: "cricao da interface de datas"

---
+++ src/main/java/org/example/DataEstilo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface DataEstilo {
+    void setDia(Integer dia);
+    void setMes(Integer mes);
+    void setAno(Integer ano);
+    Integer getDia();
+    Integer getMes();
+    Integer getAno();
+}
