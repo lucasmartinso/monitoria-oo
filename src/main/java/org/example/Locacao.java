fdc39a4bf0d471fde19f576529bf8d489b20321f
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 12:04:46 -0300

    feat: "alteracao da classe Locacao e exclusao da classe Satisfacao"

--- src/main/java/org/example/Locacao.java
+++ src/main/java/org/example/Locacao.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Locacao {
     private Cliente cliente;
     private Disco disco;
+    private Integer notaAtendimento;
+    private Integer notaFilme;
     //private Datas dataRetirada;
     //private Datas dataDevolucao;
 
@@ -25,9 +27,31 @@ public class Locacao {
         this.disco = disco;
     }
 
-    public Disco getDiscos() {
+    public Disco getDisco() {
         return this.disco;
     }
 
+    public Integer getNotaAtendimento() {
+        return notaAtendimento;
+    }
 
+    public void setNotaAtendimento(Integer notaAtendimento) {
+        if(notaAtendimento>=0 && notaAtendimento<=10) {
+            this.notaAtendimento = notaAtendimento;
+        } else {
+            throw new IllegalArgumentException("Nota tem que ser de 1 a 10");
+        }
+    }
+
+    public Integer getNotaFilme() {
+        return notaFilme;
+    }
+
+    public void setNotaFilme(Integer notaFilme) {
+        if(notaFilme>=0 && notaFilme<=10) {
+            this.notaFilme = notaFilme;
+        } else {
+            throw new IllegalArgumentException("Nota tem que ser de 1 a 10");
+        }
+    }
 }
