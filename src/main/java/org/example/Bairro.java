7ad650f56446d596a00638dbbf8c3d28f865bbc6
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 01:12:08 -0300

    feat: "classe Bairro finalizada"

--- src/main/java/org/example/Bairro.java
+++ src/main/java/org/example/Bairro.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Bairro {
+    private String nome;
+    private Cidade cidade;
+
+    public Bairro(String nome, Cidade cidade) {
+        this.setNome(nome);
+        this.setCidade(cidade);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
 }
