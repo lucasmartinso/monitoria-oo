003ce16abe76bfca18fd31ab0210ab26ef4389b1
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 00:39:36 -0300

    feat: "start project"

---
+++ src/main/java/org/example/Pais.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Pais {
+    private String name;
+
+    public Pais(String name) {
+        this.setName(name);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
