51500bd490c50277a76f7d962576a07153a05dd2
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 00:54:36 -0300

    feat: "class Estado is done"

--- src/main/java/org/example/Estado.java
+++ src/main/java/org/example/Estado.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Estado {
+    private String nome;
+    private Pais pais;
+
+    public Estado(String nome, Pais pais) {
+        this.setNome(nome);
+        this.setPais(pais);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
 }
