dc2a1dc97d55eff04c936f95b7ac7cd6b67ac03e
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 01:48:53 -0300

    feat: "criada classe filha Ator"

--- src/main/java/org/example/Ator.java
+++ src/main/java/org/example/Ator.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class Ator {
+    private Pessoa pessoa;
+    private String codigoBarras;
+    private Pais pais;
+
+    public Ator(Pessoa pessoa, String codigoBarras, Pais pais) {
+        this.setPessoa(pessoa);
+        this.setCodigoBarras(codigoBarras);
+        this.setPais(pais);
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
 }
