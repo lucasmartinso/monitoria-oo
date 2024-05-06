b54aa02d2598f77f2af82ef0a987fdd46d0c6fcf
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 09:55:49 -0300

    fix: "classe Filmes retirada de pessoa do construtor"

--- src/main/java/org/example/Ator.java
+++ src/main/java/org/example/Ator.java
@@ -1,21 +1,11 @@
 package org.example;
 
 public class Ator {
-    private Pessoa pessoa;
     private String codigoBarras;
     private Pais pais;
 
-    public Ator(Pessoa pessoa, Pais pais) {
-        this.setPessoa(pessoa);
+    public Ator(Pais pais) {
         this.setPais(pais);
-    }
-
-    public Pessoa getPessoa() {
-        return pessoa;
-    }
-
-    public void setPessoa(Pessoa pessoa) {
-        this.pessoa = pessoa;
     }
 
     public Pais getPais() {
