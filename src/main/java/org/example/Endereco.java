016f3fe2fe0e8dbef30291322f518d36d8f3744a
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 01:21:31 -0300

    feat: "classe Endereco finalizada"

--- src/main/java/org/example/Endereco.java
+++ src/main/java/org/example/Endereco.java
@@ -1,4 +1,33 @@
 package org.example;
 
 public class Endereco {
+    private Integer numero;
+    private String complemento;
+    private Rua rua;
+
+    public Endereco(Integer numero, String complemento, Rua rua) {
+        this.setNumero(numero);
+        this.setComplemento(complemento);
+        this.setRua(rua);
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public void setRua(Rua rua) {
+        this.rua = rua;
+    }
 }
