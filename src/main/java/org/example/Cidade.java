db322b31f547a7fbfa69a5f3c546e093636d5e00
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 01:09:49 -0300

    feat: "class Cidade is done"

--- src/main/java/org/example/Cidade.java
+++ src/main/java/org/example/Cidade.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Cidade {
+    private String nome;
+    private Estado estado;
+
+    public Cidade(String nome, Estado estado) {
+        this.setNome(nome);
+        this.setEstado(estado);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
 }
