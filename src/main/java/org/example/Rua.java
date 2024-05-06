0a8f1642d796aab881133e8dd031196a1473f3dd
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2024-05-06 01:15:17 -0300

    feat: "classe Rua finalizada"

--- src/main/java/org/example/Rua.java
+++ src/main/java/org/example/Rua.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Rua {
+    private String nome;
+    private Cidade cidade;
+
+    public Rua(String nome, Cidade cidade) {
+        this.setNome(nome);
+        this.setCidade(cidade);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
 }
